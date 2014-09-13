e3b77be0e1dd7cf084cda63d7cb3eb53f855164f
aidane <none@none>
2014-09-13 14:46:05 -0500

    Initial commit.

---
+++ interpreterMain.py
@@ -0,0 +1,26 @@
+def run(fp):
+    program_contents = ""
+    while True:
+        read = os.read(fp, 4096)
+        if len(read) == 0:
+            break
+        program_contents += read
+    os.close(fp)
+    program, bm = parse(program_contents)
+    mainloop(program, bm)
+
+def entry_point(argv):
+    try:
+        filename = argv[1]
+    except IndexError:
+        print "You must supply a filename"
+        return 1
+
+    run(os.open(filename, os.O_RDONLY, 0777))
+    return 0
+
+def target(*args):
+    return entry_point, None
+
+if __name__ == "__main__":
+    entry_point(sys.argv)
